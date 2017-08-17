b296fce4a93aed8ee425bd380721df5334c5b077
smartkarthik <buzykarthik@gmail.com>
2017-08-17 23:54:41 +0530

    pylint

--- geometric_progression.py
+++ geometric_progression.py
@@ -1,11 +1,28 @@
-#!/usr/bin/python3
-def GeometricProgession(series,index):\
-	#a = first term, r = common ratio
-	a = series[0]
-	r = series[1] / series[0]
-	term = a * r ** (index-1)
-	return term
+# -*- coding: utf-8 -*-
+'''
+Python Version: Python3.6
+Authored by: Akshay Kumar
+Edited by: theBuzzyCoder
+Last Edited on: Aug 17, 2017
 
-ser = [2,6,18]
-pos = 4
-print("term = ",GeometricProgession(ser,pos))
+Contains the geometric progression function
+'''
+
+
+def geometric_progession(series, index):
+    '''
+    Calculates Geometric Progression based on the series and index.
+    series is a list of integers
+    index is the nth item
+    '''
+    # a = first term, r = common ratio
+    first_term = series[0]
+    ratio = series[1] / series[0]
+    nth_item = first_term * ratio ** (index - 1)
+    return nth_item
+
+
+if __name__ == '__main__':
+    ITEM_LIST = [2, 6, 18]
+    POSITION = 4
+    print("nth item = ", geometric_progession(ITEM_LIST, POSITION))
